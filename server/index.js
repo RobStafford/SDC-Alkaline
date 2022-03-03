4a339e1aebe88ca2a358601feb07a3aaeab3e9eb
Rob Stafford <uwillgo2themoon@gmail.com>
2022-03-03 12:48:07 -0800

    Added additional queries/routes to accomodate legacy front end

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 // const readline = require('readline');
 // const stream = require('stream');
 
-const { retrieveProductById, retrieveStylesById } = require('.././database/index.js');
+const { retrieveProductById, retrieveStylesById, retrieve5Products, retrieve5Styles } = require('.././database/index.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -36,6 +36,27 @@ app.get('/products/:product_id', (req, res, next) => {
   })
 })
 
+app.get('/products/', (req, res, next) => {
+
+  //const { product_id } = req.params;
+
+  retrieve5Products((error, results) => {
+    if (error) {
+      console.log('error retriving products at server: ', error);
+      res.status(500);
+      res.end();
+    } else {
+      if (results === []) {
+        res.status(404);
+        res.end();
+      } else {
+        res.status(200).send(results);
+        res.end();
+      }
+    }
+  })
+})
+
 app.get('/styles/:product_id', (req, res, next) => {
 
   const { product_id } = req.params;
@@ -43,6 +64,27 @@ app.get('/styles/:product_id', (req, res, next) => {
   retrieveStylesById(product_id, (error, results) => {
     if (error) {
       //console.log('error retriving styles at server: ', error.status);
+      res.status(500);
+      res.end();
+    } else {
+      if (results === []) {
+        res.status(404);
+        res.end();
+      } else {
+        res.status(200).send(results);
+        res.end();
+      }
+    }
+  })
+})
+
+app.get('/styles/', (req, res, next) => {
+
+  //const { product_id } = req.params;
+
+  retrieve5Styles((error, results) => {
+    if (error) {
+      console.log('error retriving styles at server: ', error);
       res.status(500);
       res.end();
     } else {
